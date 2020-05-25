9d9dfa76e15775bc0809508782abdeb6ee9bdc54
Guido2k <Krypto.taco@gmail.com>
2020-05-24 18:18:13 -0600

    Updates

---
+++ precache-manifest.28d225026d8e15222491b7481b3a9dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840000cc04f65f5b2397e314b75067ee",
+    "url": "/https//guido2k.github.io/cryptodash/index.html"
+  },
+  {
+    "revision": "4d54954271c11c7dedbe",
+    "url": "/https//guido2k.github.io/cryptodash/static/css/main.1a0c7c8c.chunk.css"
+  },
+  {
+    "revision": "6db6e6fafb1e482b4750",
+    "url": "/https//guido2k.github.io/cryptodash/static/js/2.9818f247.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/https//guido2k.github.io/cryptodash/static/js/2.9818f247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d54954271c11c7dedbe",
+    "url": "/https//guido2k.github.io/cryptodash/static/js/main.ad8a0c7d.chunk.js"
+  },
+  {
+    "revision": "b5697cb67f51986550a6",
+    "url": "/https//guido2k.github.io/cryptodash/static/js/runtime-main.72546cca.js"
+  }
+]);
